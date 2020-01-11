49dd51831bc0b09e9e735f64234d623739917355
eternal golden bread <codylewisbrock@gmail.com>
2020-01-11 14:50:40 -0800

    adds validation for fields, cleans up the code, redoes some of the styling

--- app/routes/apiRoutes.js
+++ app/routes/apiRoutes.js
@@ -2,22 +2,18 @@ var friendsArr = require("../data/friends");
 
 module.exports = function(app) {
 
-  //Not sure if this one works...
-  // Displays all friends
+
   app.get("/app/data/friends", function (req, res) {
       return res.json(friendsArr);
   });
 
-
-
-
   // Create New Person and find their friend - takes in JSON input
   app.post("/app/data/friends", function (req, res) {
-    // req.body hosts is equal to the JSON post sent from the user
-    // This works because of our body parsing middleware
-    var newFriend = req.body;
 
-    //Code here to match the friends...
+    var newFriend = req.body;
+    
+
+    //Code here to match the user to their friend...
     let minDiff = Infinity;
     let closestMatch
     for (let i = 0; i < friendsArr.length; i++) {
@@ -29,15 +25,15 @@ module.exports = function(app) {
         closestMatch = friendsArr[i];
         minDiff = currDiff;
       }
-      console.log("Compared you to: ", friendsArr[i].name);
-      console.log("Your score difference was: ", currDiff);
-      console.log("------")
+      // console.log("Compared you to: ", friendsArr[i].name);
+      // console.log("Your score difference was: ", currDiff);
+      // console.log("------")
     }
 
-    console.log("*****************************")
-    console.log("YOUR NEW FRIEND IS: ", closestMatch);
-    console.log("Your score difference was only: ", minDiff);
-    console.log("*****************************")
+    // console.log("*****************************")
+    // console.log("YOUR NEW FRIEND IS: ", closestMatch);
+    // console.log("Your score difference was only: ", minDiff);
+    // console.log("*****************************")
 
     friendsArr.push(newFriend);
 
